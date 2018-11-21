a7e2c8208607de626de379e2e2d77f517fd74fa0
Allan Daemon <rea.aft@gmail.com>
2018-11-21 18:28:59 -0200

    browser_provider: got something working

--- browser.py
+++ browser.py
@@ -4,19 +4,48 @@ import os
 import re
 from pathlib import Path
 
+import xdg
+from xdg.DesktopEntry import DesktopEntry
+
 APPLICATIONS_DIRS = ['/usr/share/applications']
 APPLICATIONS_DIRS = list(map(Path, APPLICATIONS_DIRS))
 
-XDG_CAT_RE = r'Categories=(.*?)WebBrowser;(.*?)'
-browser_re = re.compile(XDG_CAT_RE)
+# XDG_CAT_RE = r'Categories=(.*?)WebBrowser;(.*?)'
+# browser_re = re.compile(XDG_CAT_RE)
 
-def get_brower_list():
-	browsers_xdg_files = []
+def get_browser_list():
+	de_list = get_browser_desktop_list()
+	return [ (de.getName(), de.getIcon(), de)
+		for de in de_list.values() ]
+
+def get_browser_desktop_list():
+	browsers_xdg_files = {}
+
 	for DIR in APPLICATIONS_DIRS:
 		for f in DIR.iterdir():
 			if not f.is_file(): continue
-			if browser_re.search(f.read_text()):
-				browsers_xdg_files.append(f)
-				print(browsers_xdg_files[-1])
+			try: de = DesktopEntry(f)
+			except xdg.Exceptions.ParsingError: continue
 
-get_brower_list()
+			if 'WebBrowser' in de.getCategories():
+				browsers_xdg_files[f] = de
+				print(f)
+	return browsers_xdg_files
+
+# def get_browser_xdg_files_list():
+# 	browsers_xdg_files = []
+# 	for DIR in APPLICATIONS_DIRS:
+# 		for f in DIR.iterdir():
+# 			if not f.is_file(): continue
+# 			try: de = DesktopEntry(f)
+# 			except xdg.Exceptions.ParsingError: continue
+# 			if 'WebBrowser' in de.getCategories():
+# 			# if browser_re.search(f.read_text()):
+# 				browsers_xdg_files.append([f, de])
+# 				print(browsers_xdg_files[-1])
+# 	return browsers_xdg_files
+
+
+l = get_browser_list()
+# ff = list(l.values())[0]
+
