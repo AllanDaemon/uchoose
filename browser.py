0cb6c15531571cea34f140c236830a714b56ddb8
Allan Daemon <rea.aft@gmail.com>
2018-11-21 18:02:36 -0200

    browser: getting xdg files already

--- browser.py
+++ browser.py
@@ -1,11 +1,11 @@
-#!/usr/bin/usr python3
+#!/usr/bin/env python3
 
 import os
 import re
-from path import path
+from pathlib import Path
 
 APPLICATIONS_DIRS = ['/usr/share/applications']
-APPLICATIONS_DIRS = list(map(path, APPLICATIONS_DIRS))
+APPLICATIONS_DIRS = list(map(Path, APPLICATIONS_DIRS))
 
 XDG_CAT_RE = r'Categories=(.*?)WebBrowser;(.*?)'
 browser_re = re.compile(XDG_CAT_RE)
@@ -13,12 +13,10 @@ browser_re = re.compile(XDG_CAT_RE)
 def get_brower_list():
 	browsers_xdg_files = []
 	for DIR in APPLICATIONS_DIRS:
-		files = os.listdir(DIR)
-		for f_name in files:
-			with open(DIR / f_name) as f:
-				content  = f.read()
-				if browser_re.search(content):
-					browsers_xdg_files.append(DIR / f_name)
-					print(browsers_xdg_files[0])
+		for f in DIR.iterdir():
+			if not f.is_file(): continue
+			if browser_re.search(f.read_text()):
+				browsers_xdg_files.append(f)
+				print(browsers_xdg_files[-1])
 
 get_brower_list()
