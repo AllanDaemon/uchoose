cd004645bc7a5bf1c8c9a7d6f0d66faf5e75e4a9
Allan Daemon <rea.aft@gmail.com>
2018-11-22 10:59:27 -0200

    cli: choice input validated

--- cli.py
+++ cli.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python3
 
+def get_choice(browser_list):
+	choice = input(f'({browser_list[0].name}): ')
+	
+	if not choice: return None
+
+	try:
+		choice = int(choice)
+	except:
+		return get_choice(browser_list)
+	
+	if choice < 0 or choice >= len(browser_list):
+		return get_choice(browser_list)
+
+	return choice
+
+
+
 def cli(url, browser_list, default=0):
 	print(f"Open: {url}")
-	# browser_list_ex = ['Firefox', 'Chrome']
+
 	for i, b in enumerate(browser_list):
 		print(f"\t{i}  {b.name}")
-	choice = input(f'({browser_list[0].name}): ')
-	print('CHOICE:', repr(choice))
 	
-	if choice: choice = int(choice)
-	else: choice = default
+	choice = get_choice(browser_list)
+	if choice is None: choice = default
 
 	return choice, browser_list[choice]
