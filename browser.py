82947baede842ade313674d0643c1f944d7cab7b
Allan Daemon <rea.aft@gmail.com>
2018-11-21 22:27:50 -0200

    browser: use named tuple, add exec to info

--- browser.py
+++ browser.py
@@ -8,10 +8,12 @@ from xdg.DesktopEntry import DesktopEntry
 APPLICATIONS_DIRS = ['/usr/share/applications']
 APPLICATIONS_DIRS = list(map(Path, APPLICATIONS_DIRS))
 
+BrowserEntry = namedtuple('BrowserEntry', ['name', 'icon', 'exec', 'de'])
+
 
 def get_browser_list():
 	de_list = get_browser_desktop_list()
-	return [ (de.getName(), de.getIcon(), de)
+	return [ BrowserEntry(de.getName(), de.getIcon(), de.getExec(), de)
 		for de in de_list ]
 
 def get_browser_desktop_list():
