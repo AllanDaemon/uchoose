e6218bf74a60bfb480c296dcbffcff963e983827
Allan Daemon <rea.aft@gmail.com>
2018-11-22 11:16:56 -0200

    qt: cleanup, args adjustment

--- gui_qt.py
+++ gui_qt.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
 
-import os
 from PySide2 import QtCore, QtWidgets, QtGui
 
 FONT_SIZE = 20
 ICON_SIZE = 32, 32
 
 class UOR(QtWidgets.QWidget):
-	def __init__(self):
+	def __init__(self, url, browser_list, default):
 		QtWidgets.QWidget.__init__(self)
+		self.url = url
+		self.browser_list = browser_list
+		self.default = default
+
+		self.init_ui()
+		self.show()
+
+	def init_ui(self):
 		self.layout = QtWidgets.QVBoxLayout()
 		self.setLayout(self.layout)
 
-		self.url = QtWidgets.QLabel("http://example.com/this/is.a.url?all=right")
-		self.url.setFont(QtGui.QFont(fontSize=20))
+		# self.url = QtWidgets.QLabel("http://example.com/this/is.a.url?all=right")
+		self.url = QtWidgets.QLabel(self.url)
 
 		icon1 = QtGui.QIcon('/usr/share/icons/hicolor/256x256/apps/firefox.png')
 		icon2 = QtGui.QIcon('/usr/share/icons/hicolor/256x256/apps/google-chrome-beta.png')
@@ -29,13 +36,10 @@ class UOR(QtWidgets.QWidget):
 
 
 def gui_qt(url, browser_list, default):
-# if __name__ == "__main__":
+	app = QtWidgets.QApplication()
+
+	widget = UOR(url, browser_list, default)
+
 	import sys
-	app = QtWidgets.QApplication(sys.argv)
-
-	widget = UOR()
-	widget.show()
-
 	sys.exit(app.exec_())
 	# return chosen
-
