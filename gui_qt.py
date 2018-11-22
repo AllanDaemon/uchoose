c28c7fe919d04bbad099ec385a969df9bcb4e116
Allan Daemon <rea.aft@gmail.com>
2018-11-22 11:23:39 -0200

    qt: using browser list; missing icons

--- gui_qt.py
+++ gui_qt.py
@@ -19,20 +19,20 @@ class UOR(QtWidgets.QWidget):
 		self.layout = QtWidgets.QVBoxLayout()
 		self.setLayout(self.layout)
 
-		# self.url = QtWidgets.QLabel("http://example.com/this/is.a.url?all=right")
 		self.url = QtWidgets.QLabel(self.url)
-
-		icon1 = QtGui.QIcon('/usr/share/icons/hicolor/256x256/apps/firefox.png')
-		icon2 = QtGui.QIcon('/usr/share/icons/hicolor/256x256/apps/google-chrome-beta.png')
-
-		self.entries = [
-			QtWidgets.QPushButton(icon1, 'Firefox'),
-			QtWidgets.QPushButton(icon2, 'Chrome'),
-		]
-
 		self.layout.addWidget(self.url)
-		self.layout.addWidget(self.entries[0])
-		self.layout.addWidget(self.entries[1])
+
+		self.entries = []
+		for name, icon, _, _ in self.browser_list:
+			print("QT:", name, icon)
+
+			icon = QtGui.QIcon('/usr/share/icons/hicolor/256x256/apps/firefox.png')
+			icon = QtGui.QIcon('/usr/share/icons/hicolor/256x256/apps/google-chrome-beta.png')
+
+			btn = QtWidgets.QPushButton(icon, name, self)
+
+			self.entries.append(btn)
+			self.layout.addWidget(btn)
 
 
 def gui_qt(url, browser_list, default):
