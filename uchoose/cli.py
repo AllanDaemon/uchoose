b91f3424844deada2d97e40b358f6f502060781e
Allan Daemon <rea.aft@gmail.com>
2018-12-29 06:16:15 -0200

    shebang fix (use env)

--- uchoose/cli.py
+++ uchoose/cli.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 #@TODO: Use colors/bold
 #@TODO: use ncurses or something alike
--- uchoose/gui_qt.py
+++ uchoose/gui_qt.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import signal
 from PySide2 import QtCore, QtWidgets, QtGui
