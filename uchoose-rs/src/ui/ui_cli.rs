b6d7086d04041e65210d4224c2c6e02094e10ead
Allan Daemon <git@allan.gg>
2024-09-03 19:19:04 -0300

    ui_cli: working

--- uchoose-rs/src/ui/ui_cli.rs
+++ uchoose-rs/src/ui/ui_cli.rs
@@ -1,25 +1,34 @@
-use std::io;
-
+use super::Choice;
 use crate::providers::BrowserEntry;
 
-use super::Choice;
-
-fn get_choice(browser_list: &Vec<BrowserEntry>) -> Result<usize, ()> {
+fn get_choice(browser_list: &Vec<BrowserEntry>) -> Option<usize> {
     let mut input_line = String::new();
 
+    // Print prompt
     print!("({}): ", browser_list[0].name);
 
-    if io::stdin().read_line(&mut input_line).is_err() {
+    // Read line
+    let _input_result = std::io::stdin().read_line(&mut input_line);
+    if _input_result.is_err() {
         println!("No answer read");
         return get_choice(browser_list);
+    } else if _input_result.unwrap() == 0 {
+        // EOF -> just exit the program
+        return None;
     }
+
+    // Parse number
     if let Ok(choice) = input_line.trim().parse() {
-        if choice < 0 || choice >= browser_list.len() {
+        // Check number range
+        if choice >= browser_list.len() {
             println!("Wrong choice! Must be from 0 up to {}", browser_list.len());
             return get_choice(browser_list);
         }
-        return Ok(choice);
+
+        // Number ok
+        return Some(choice);
     } else {
+        // Failed parsing number
         println!("Wrong choice (not a number)!");
         return get_choice(browser_list);
     }
@@ -30,14 +39,13 @@ pub fn chooser(url: String, browser_list: &Vec<BrowserEntry>, default: Choice) -
 
     for (i, b) in browser_list.iter().enumerate() {
         let prefix = if i == default { '*' } else { ' ' };
-
         println!("  {}{}  {}", prefix, i, b.name)
     }
 
-    if let Ok(choice) = get_choice(browser_list) {
-		choice
-	} else {
-		println!("Error getting choice; using default");
-		default
-	}
+    if let Some(choice) = get_choice(browser_list) {
+        choice
+    } else {
+        println!("Error getting choice; using default");
+        default
+    }
 }
