cbec0958898c1bbe7aa66126090c35765844d7d2
Allan Daemon <git@allan.gg>
2024-09-05 20:45:24 -0300

    choice & default: [part 1] choice to be Option<>;  rename default -> default_option

--- uchoose-rs/src/main.rs
+++ uchoose-rs/src/main.rs
@@ -8,7 +8,7 @@ use providers::BrowserEntry;
 mod providers;
 mod ui;
 
-const DEFAULT_OPTION: i32 = 0;
+const DEFAULT_OPTION: i32 = 0; // Copy to clipboard entry
 static DBG_URL: &str = "http://example.com/this/is.a.url?all=right";
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
@@ -24,8 +24,8 @@ enum UI {
 #[command(version, about, long_about = None)]
 struct Cli {
     #[arg(default_value_t = DEFAULT_OPTION)]
-    #[arg(short, long)]
-    default: i32,
+    #[arg(short, long = "default")]
+    default_option: i32,
 
     #[arg(value_enum)]
     #[arg(default_value_t = UI::Relm)]
@@ -74,29 +74,34 @@ fn main() {
     println!("\tURL: {}", cli.url);
 
     match cli.ui {
-        UI::CLI => return choose_and_execute(ui::ui_cli::chooser, cli.url, cli.default),
-        UI::GTK => return choose_and_execute(ui::ui_gtk4::chooser, cli.url, cli.default),
-        UI::Relm => return choose_and_execute(ui::ui_relm4::chooser, cli.url, cli.default),
+        UI::CLI => return choose_and_execute(ui::ui_cli::chooser, cli.url, cli.default_option),
+        UI::GTK => return choose_and_execute(ui::ui_gtk4::chooser, cli.url, cli.default_option),
+        UI::Relm => return choose_and_execute(ui::ui_relm4::chooser, cli.url, cli.default_option),
         UI::Iced => unimplemented!(),
         UI::TestProviders => return providers::main_dev(),
     }
 }
 
 // default: 0 is the 1st in the list; 1 is the 2nd; -1 is the last; -2 is the second to last;
-fn choose_and_execute(chooser: ui::Chooser, url: String, default: i32) {
+fn choose_and_execute(chooser: ui::Chooser, url: String, default_option: i32) {
     // Choose
     let browser_list: Vec<BrowserEntry> = providers::get_browsers_list();
 
-    let _default: ui::Choice = if default < 0 {
-        (browser_list.len() as i32 + default).max(0)
+    let default_option: ui::ChoiceIndex = if default_option < 0 {
+        (browser_list.len() as i32 + default_option).max(0)
     } else {
-        default
-    } as ui::Choice;
+        default_option
+    } as ui::ChoiceIndex;
 
-    let choice = chooser(url, &browser_list, _default);
-    let browser = browser_list[choice].clone();
+    let choice = chooser(url, &browser_list, default_option);
+    if let None = choice {
+        println!("Choosing cancelled. No action will be taken.");
+        return;
+    }
 
-    println!("CHOICE: {:?} [{:#?}]", choice, browser);
+    let entry = browser_list[choice.unwrap()].clone();
+
+    println!("CHOICE: {:?} [{:#?}]", choice, entry);
 
     // Execute
 }
--- uchoose-rs/src/ui/mod.rs
+++ uchoose-rs/src/ui/mod.rs
@@ -1,6 +1,11 @@
-pub type Chooser =
-    fn(url: String, browser_list: &Vec<crate::providers::BrowserEntry>, default: Choice) -> Choice;
-pub type Choice = usize;
+pub type ChoiceIndex = usize;
+pub type Choice = Option<ChoiceIndex>;
+
+pub type Chooser = fn(
+    url: String,
+    browser_list: &Vec<crate::providers::BrowserEntry>,
+    default_option: ChoiceIndex,
+) -> Choice;
 
 pub mod ui_cli;
 
