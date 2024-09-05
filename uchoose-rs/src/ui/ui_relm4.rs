26ceb5af3e1b27c9c174698231bfd4e11bd25212
Allan Daemon <git@allan.gg>
2024-09-05 20:19:56 -0300

    cargo: add relm4

---
+++ uchoose-rs/src/ui/ui_relm4.rs
@@ -0,0 +1,8 @@
+use super::Choice;
+use crate::providers::BrowserEntry;
+
+pub fn chooser(url: String, browser_list: &Vec<BrowserEntry>, default: Choice) -> Choice {
+    println!("Relm4 Open: {}", url);
+
+    0
+}
