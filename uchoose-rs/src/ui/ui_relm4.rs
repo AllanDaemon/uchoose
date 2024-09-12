b6f9a2d0ee43ae6fc6d4381d0e798dfcf541a0e4
Allan Daemon <git@allan.gg>
2024-09-12 04:03:27 -0300

    relm: BIG revamp using relm macros

--- uchoose-rs/src/ui/ui_relm4.rs
+++ uchoose-rs/src/ui/ui_relm4.rs
@@ -2,8 +2,9 @@ use std::cell::RefCell;
 use std::rc::Rc;
 
 use gtk::glib::clone;
-use gtk::prelude::*;
-use relm4::prelude::*;
+use gtk::{prelude::*, Widget};
+use relm4::factory::FactoryVecDeque;
+use relm4::{prelude::*, RelmIterChildrenExt};
 
 use super::{Choice, ChoiceIndex};
 use crate::get_cli_args;
@@ -12,30 +13,12 @@ use crate::ui::ui_gtk4::get_padding_size;
 
 const APP_ID: &str = "gg.allan.uchoose.rs.relm4";
 
-struct UchooseApp {
-    result: Rc<RefCell<Choice>>, // The way to get write the result back to us
-}
-
-#[derive(Debug)]
-enum InputMsg {
-    Chosen(ChoiceIndex),
-    Cancelled,
-}
-
-#[derive(Debug, Clone)]
-struct UchooseParams {
-    url: String,
-    browser_list: Vec<BrowserEntry>,
-    default_option: ChoiceIndex,
-    result: Rc<RefCell<Choice>>, // Used as intermediary to build in the model
-}
-
 pub fn chooser(url: &str, browser_list: &Vec<BrowserEntry>, default_option: ChoiceIndex) -> Choice {
     println!("Relm4 Open: {}", url);
 
     let result = Rc::new(RefCell::new(None));
 
-    let choose_params: UchooseParams = UchooseParams {
+    let choose_params = UchooseParams {
         url: url.to_string(),
         browser_list: browser_list.clone(),
         default_option: default_option.clone(),
@@ -53,19 +36,123 @@ pub fn chooser(url: &str, browser_list: &Vec<BrowserEntry>, default_option: Choi
     return *result.borrow();
 }
 
-struct AppWidgets {}
+/*                */
+// Button Factory //
 
+#[derive(Debug, Clone)]
+struct ChoiceButton {
+    index: ChoiceIndex,
+    name: String,
+    icon_name: String,
+    exec: Option<String>,
+}
+
+#[derive(Debug)]
+enum ChoiceButtonOutput {
+    Choice(ChoiceIndex),
+}
+
+#[relm4::factory]
+impl FactoryComponent for ChoiceButton {
+    type Init = ChoiceButton;
+    type Input = ();
+    type Output = ChoiceButtonOutput;
+    type CommandOutput = ();
+    type ParentWidget = gtk::Box;
+
+    view! {
+        #[root]
+        #[name="btn_wrapper_box"]
+        gtk::Box{
+            #[name="btn"]
+            gtk::Button {
+                set_tooltip_text: self.exec.as_deref(),
+                // This's the default in pure GTK builder, not here in macro
+                set_hexpand: true,
+
+                connect_clicked[sender, idx=self.index.clone()] => move |_| {
+                    sender.output(ChoiceButtonOutput::Choice(idx))
+                        .expect("Error sending ChoiceButtonOutput::Choice()");
+                },
+
+                #[name="btn_inner_box"]
+                gtk::Box{
+                    set_orientation: gtk::Orientation::Horizontal,
+                    set_spacing: get_padding_size(),
+
+                    #[name="btn_icon"]
+                    gtk::Image {
+                        set_icon_name: Some(&self.icon_name),
+                        set_icon_size: gtk::IconSize::Large,
+                    },
+
+                    #[name="btn_label"]
+                    gtk::Label{
+                        set_label: &self.name.to_string(),
+                    },
+                },
+            }
+        }
+    }
+
+    fn init_model(value: Self::Init, _index: &DynamicIndex, _sender: FactorySender<Self>) -> Self {
+        value.clone()
+    }
+}
+
+// Main Window //
+
+#[derive(Debug)]
+struct UchooseApp {
+    url: String,
+    result: Rc<RefCell<Choice>>, // Used as intermediary to build in the model
+    choice_buttons: FactoryVecDeque<ChoiceButton>,
+}
+
+#[derive(Debug, Clone)]
+struct UchooseParams {
+    url: String,
+    browser_list: Vec<BrowserEntry>,
+    default_option: ChoiceIndex,
+    result: Rc<RefCell<Choice>>, // Used as intermediary to build in the model
+}
+
+#[derive(Debug)]
+enum InputMsg {
+    Chosen(ChoiceIndex),
+    Cancelled,
+}
+
+#[relm4::component]
 impl SimpleComponent for UchooseApp {
     type Init = UchooseParams;
     type Input = InputMsg;
     type Output = ();
-    type Widgets = AppWidgets;
-    type Root = gtk::ApplicationWindow;
 
-    fn init_root() -> Self::Root {
-        let win = gtk::ApplicationWindow::builder().title("uChoose").build();
-        super::ui_gtk4::set_scale(&win, get_cli_args().ui_scale);
-        win
+    view! {
+        gtk::ApplicationWindow {
+            set_title: Some("uChoose"),
+
+            #[name="box_win"]
+            gtk::Box {
+                set_orientation: gtk::Orientation::Vertical,
+                set_spacing: padding_size,
+                set_margin_all: padding_size,
+
+                #[name="url_label"]
+                gtk::Label {
+                    set_label: &model.url,
+                    set_margin_all: padding_size,
+                    set_selectable: true, // Set windows focus latter to avoid starting selected
+                },
+
+                #[local_ref]
+                choice_buttons_box -> gtk::Box {
+                    set_orientation: gtk::Orientation::Vertical,
+                    set_spacing: padding_size,
+                }
+            }
+        }
     }
 
     fn init(
@@ -74,67 +161,50 @@ impl SimpleComponent for UchooseApp {
         sender: ComponentSender<Self>,
     ) -> ComponentParts<Self> {
         chooser_connect_cancel(sender.clone(), window.clone());
+        super::ui_gtk4::set_scale(&window, get_cli_args().ui_scale);
 
-        let model = UchooseApp {
-            result: init_params.result,
-        };
-
-        let padding_size: i32 = get_padding_size();
-
-        let url_label = gtk::Label::new(Some(&init_params.url));
-        url_label.set_margin_all(padding_size);
-        url_label.set_selectable(true); // Set windows focus latter to avoid starting selected
-
-        let vbox = gtk::Box::builder()
-            .orientation(gtk::Orientation::Vertical)
-            .spacing(padding_size)
-            .build();
-        vbox.set_margin_all(padding_size);
-        vbox.append(&url_label);
-        window.set_child(Some(&vbox));
-
-        for (idx_btn, entry) in init_params.browser_list.iter().enumerate() {
-            let label = gtk::Label::new(Some(&entry.name));
-
-            let icon = gtk::Image::builder()
-                .icon_name(entry.icon.to_owned())
-                .icon_size(gtk::IconSize::Large)
-                .build();
-
-            let btn_box = gtk::Box::builder()
-                .orientation(gtk::Orientation::Horizontal)
-                .spacing(padding_size)
-                .build();
-            btn_box.append(&icon);
-            btn_box.append(&label);
-
-            let btn_tooltip_text = match &entry.action {
-                EntryAction::Exec(s) => s,
-                _ => &String::new(),
-            };
-
-            let btn = gtk::Button::builder()
-                .child(&btn_box)
-                .tooltip_text(btn_tooltip_text)
-                .build();
-
-            vbox.append(&btn);
-
-            // Select the default option to remove the selection from url_label
-            if idx_btn == init_params.default_option {
-                gtk::prelude::RootExt::set_focus(&window, Some(&btn));
-            }
-
-            btn.connect_clicked(clone!(
-                #[strong]
-                sender,
-                move |_| {
-                    sender.input(InputMsg::Chosen(idx_btn));
+        // Choice buttons factory
+        let mut choice_buttons: FactoryVecDeque<ChoiceButton> = FactoryVecDeque::builder()
+            .launch(gtk::Box::default())
+            .forward(sender.input_sender(), |output| match output {
+                ChoiceButtonOutput::Choice(index) => {
+                    println!("FOWARD EVENT: {output:#?}");
+                    InputMsg::Chosen(index)
                 }
-            ));
+            });
+
+        // populate choice buttons
+        {
+            let mut choice_buttons_guard = choice_buttons.guard();
+
+            for (idx_btn, entry) in init_params.browser_list.iter().enumerate() {
+                choice_buttons_guard.push_back(ChoiceButton {
+                    index: idx_btn,
+                    name: entry.name.to_owned().to_string(),
+                    icon_name: entry.icon.to_owned().to_string(),
+                    exec: match entry.action.to_owned() {
+                        EntryAction::Exec(cmd) => Some(cmd),
+                        _ => None,
+                    },
+                });
+            }
         }
 
-        let widgets = AppWidgets {};
+        let model = UchooseApp {
+            url: init_params.url,
+            result: init_params.result,
+            choice_buttons: choice_buttons,
+        };
+
+        let choice_buttons_box = model.choice_buttons.widget();
+
+        let padding_size = get_padding_size();
+
+        let widgets = view_output!();
+
+        // focus default btn
+        chooser_set_focus_on_button(&window, &choice_buttons_box, init_params.default_option);
+
         ComponentParts { model, widgets }
     }
 
@@ -154,6 +224,17 @@ impl SimpleComponent for UchooseApp {
         *self.result.borrow_mut() = choice;
         relm4::main_application().quit();
     }
+}
+
+fn chooser_set_focus_on_button(
+    window: &gtk::ApplicationWindow,
+    choice_buttons_box: &gtk::Box,
+    index: ChoiceIndex,
+) {
+    let box_children: Vec<Widget> = choice_buttons_box.iter_children().collect();
+    let default_btn_widget = &box_children[index];
+    let default_btn = &default_btn_widget.first_child().unwrap();
+    gtk::prelude::RootExt::set_focus(window, Some(default_btn));
 }
 
 fn chooser_connect_cancel(sender: ComponentSender<UchooseApp>, window: gtk::ApplicationWindow) {
